537c28b620ac34ef119690fbd1b57c9dd5fec608
Babak Jafari <babak.jafari4@gmail.com>
2019-06-26 23:33:58 +0430

    migration enabled

---
+++ GigHub01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GigHub01.Startup))]
+namespace GigHub01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
