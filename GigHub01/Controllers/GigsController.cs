972196b7539df8e800ae4bd6b15ff227477848e4
Babak Jafari <babak.jafari4@gmail.com>
2019-06-27 18:05:52 +0430

    All files Added commited

---
+++ GigHub01/Controllers/GigsController.cs
@@ -0,0 +1,51 @@
+﻿using GigHub01.Models;
+using GigHub01.ViewModels;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub01.Controllers
+{
+    [Authorize]
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel()
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel gigFormViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                gigFormViewModel.Genres = _context.Genres.ToList();
+                return View("Create", gigFormViewModel);
+
+            }
+
+            var gig = new Gig()
+            {
+                ArtistId = User.Identity.GetUserId(),
+                DateTime = gigFormViewModel.GetDateTime(),
+                GenreId = gigFormViewModel.Genre,
+                Venue = gigFormViewModel.Venue
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ GigHub01/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,30 @@
+﻿using GigHub01.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace GigHub01.ViewModels
+{
+    public class GigFormViewModel
+    {
+        [Required]
+        public string Venue { get; set; }
+
+        [Required]
+        [FutureDate]
+        public string Date { get; set; }
+
+        [Required]
+        [ValidTime]
+        public string Time { get; set; }
+
+        [Required]
+        public byte Genre { get; set; }
+        [Required]
+        public IEnumerable<Genre> Genres { get; set; }
+
+        public DateTime GetDateTime() => DateTime.Parse($"{Date} {Time}");
+
+
+    }
+}
