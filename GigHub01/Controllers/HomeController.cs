8115c7cf961738f197717fefc5d7ba024e6b7409
Babak Jafari <babak.jafari4@gmail.com>
2019-06-27 21:25:25 +0430

    All Files edited for adding name to user entity

--- GigHub01/Controllers/HomeController.cs
+++ GigHub01/Controllers/HomeController.cs
@@ -1,16 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using GigHub01.Models;
+using System;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace GigHub01.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
-            return View();
+            var upcomingGigs = _context.Gigs.
+                Include(i => i.Artist).
+                Where(w => w.DateTime > DateTime.Now);
+            return View(upcomingGigs);
         }
 
         public ActionResult About()
