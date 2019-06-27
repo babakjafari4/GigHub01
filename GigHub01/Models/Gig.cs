73d4fec811a807bfc55b94b717c328bc15fca27c
Babak Jafari <babak.jafari4@gmail.com>
2019-06-27 18:03:44 +0430

    All files update commited

--- GigHub01/Models/Gig.cs
+++ GigHub01/Models/Gig.cs
@@ -7,8 +7,9 @@ namespace GigHub01.Models
     {
         public int Id { get; set; }
 
-        [Required]
         public ApplicationUser Artist { get; set; }
+        [Required]
+        public string ArtistId { get; set; }
 
         public DateTime DateTime { get; set; }
 
@@ -16,7 +17,8 @@ namespace GigHub01.Models
         [MaxLength(255)]
         public string Venue { get; set; }
 
-        [Required]
         public Genre Genre { get; set; }
+        [Required]
+        public byte GenreId { get; set; }
     }
 }
